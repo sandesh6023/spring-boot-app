ac5483a50fcf140222e270f3adb138760c70a1be
sandesh m <sandesh.m@ge.com>
2020-06-14 14:06:36 +0530

    Reading path param api added

--- src/main/java/com/sample/postgress/controller/ApplicationController.java
+++ src/main/java/com/sample/postgress/controller/ApplicationController.java
@@ -5,15 +5,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sample.postgress.entity.Employee;
-
 import com.sample.postgress.entity.Sso;
 import com.sample.postgress.service.EmployeeService;
+
 
 @RestController
 @RequestMapping("/postgressApp")
@@ -37,6 +39,13 @@ public class ApplicationController {
 	
 	}
 	
+	
+	@RequestMapping(value = "/branch/{branchname}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json", headers = "Accept=application/json")
+	public void getBranchname(@PathVariable("branchname") String branch ) {	
+		System.out.print("----->"+branch);
+//		return iPunchlistDashboardService.getPunchListDashboard(punchlistdashboarddropDownDTO);
+	}
+
 	
 	
 	
